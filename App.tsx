0dc1ad0f3d91ba7cb1f78d02eb1fe55aaf64b07d
jakub.bartosik@mayeryn.com <jbartos13@gmail.com>
2021-09-20 20:39:50 +0200

    added style to the App container

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { DrawerNavigation } from "./components/Navigation/DrawerNavigation";
 
 export default function App() {
@@ -10,19 +10,18 @@ export default function App() {
     uri: "http://localhost:4000/graphql",
   });
   return (
-    <ApolloProvider client={client}>
-      <NavigationContainer>
-        <DrawerNavigation />
-      </NavigationContainer>
-    </ApolloProvider>
+    <View style={styles.container}>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <DrawerNavigation />
+        </NavigationContainer>
+      </ApolloProvider>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
